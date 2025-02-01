859313a9c07163db99e8dcf6ff8b151c8f810668
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-02-01 19:15:01 +0700

    Add func InitEngine

--- initializers/init.go
+++ initializers/init.go
@@ -2,10 +2,17 @@ package initializers
 
 import (
     "gin-rest-api/initializers/db"
+    "gin-rest-api/initializers/engine"
     "gin-rest-api/initializers/env"
+    "github.com/gin-gonic/gin"
+    "gorm.io/gorm"
 )
+
+var DB *gorm.DB
+var ENGINE *gin.Engine
 
 func Init() {
     env.InitEnv()
-    db.InitDB()
+    DB = db.InitDB()
+    ENGINE = engine.InitEngine()
 }
