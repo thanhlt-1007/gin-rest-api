93f69632d30a001f22b01e682bb769b12b2c5899
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-02-02 09:30:37 +0700

    Add validation_error_serializer package

---
+++ serializers/errors/validation_error_serializer/serializer.go
@@ -0,0 +1,27 @@
+package validation_error_serializer
+
+import (
+    "fmt"
+    "gin-rest-api/responses/errors/field_error_response"
+    "gin-rest-api/responses/errors/validation_error_response"
+    "gin-rest-api/serializers/errors/field_error_serializer"
+    "github.com/go-playground/validator/v10"
+)
+
+func Serializer(err validator.ValidationErrors) validation_error_response.Response {
+    message := fmt.Sprintf("VALIDATION_ERROR: %s", err.Error())
+    code := "VALIDATION_ERROR"
+    var errs []field_error_response.Response
+    for _, e := range err {
+        errs = append(
+            errs,
+            field_error_serializer.Serializer(e),
+        )
+    }
+
+    return validation_error_response.Response {
+        Message: message,
+        Code: code,
+        Errors: errs,
+    }
+}
