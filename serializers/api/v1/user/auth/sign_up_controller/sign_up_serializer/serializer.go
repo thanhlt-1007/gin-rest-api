0cc34a572d4e772710bc58be029a6ffa27064456
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-02-10 22:45:51 +0700

    Update repositories

--- serializers/api/v1/user/auth/sign_up_controller/sign_up_serializer/serializer.go
+++ serializers/api/v1/user/auth/sign_up_controller/sign_up_serializer/serializer.go
@@ -5,7 +5,7 @@ import (
     "gin-rest-api/responses/api/v1/user/auth/sign_up_controller/sign_up_response"
 )
 
-func Serializer(token models.Token) sign_up_response.Response {
+func Serializer(token *models.Token) sign_up_response.Response {
     data := sign_up_response.Data{
         ID: token.ID,
         AccessToken: token.AccessToken,
--- services/api/v1/user/auth/sign_up_controller/sign_up_service/perform.go
+++ services/api/v1/user/auth/sign_up_controller/sign_up_service/perform.go
@@ -6,15 +6,25 @@ import (
     "gin-rest-api/repositories/user_repository"
     "gin-rest-api/requests/api/v1/user/auth/sign_up_controller/sign_up_request"
     "gin-rest-api/utils/password"
+    fmt_util "gin-rest-api/utils/fmt"
+    "log"
 )
 
-func Perform(request sign_up_request.Request) models.Token {
+func Perform(request sign_up_request.Request) *models.Token {
     user := models.User {
         Email: request.Email,
         EncryptedPassword: password.Encrypt(request.Password),
     }
-    user_repository.CreateUser(&user)
-    token := token_repository.CreateUserToken(user)
+    _, err := user_repository.CreateUser(&user)
+    if err != nil {
+        log.Printf("sign_up_service.Perform error\n%s", fmt_util.SprintfError(err))
+        panic(err)
+    }
+    token, err := token_repository.CreateUserToken(&user)
+    if err != nil {
+        log.Printf("sign_up_service.Perform error\n%s", fmt_util.SprintfError(err))
+        panic(err)
+    }
 
     return token
 }
